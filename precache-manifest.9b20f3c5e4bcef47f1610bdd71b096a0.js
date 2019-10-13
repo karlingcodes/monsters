d5a10a023f33853af0ab82a4f103ac702b18a98e
Ho Kar Ling <8458817+karlingcodes@users.noreply.github.com>
2019-10-13 22:54:11 +0800

    Updates

---
+++ precache-manifest.9b20f3c5e4bcef47f1610bdd71b096a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df618c7d71c84d7d39b601c33df5f6d1",
+    "url": "/karlingcodes/monsters.git/index.html"
+  },
+  {
+    "revision": "b8c60ef05d9d550bb0e6",
+    "url": "/karlingcodes/monsters.git/static/css/main.9fba2553.chunk.css"
+  },
+  {
+    "revision": "a2079fc446d24f59ff82",
+    "url": "/karlingcodes/monsters.git/static/js/2.462f81e5.chunk.js"
+  },
+  {
+    "revision": "b8c60ef05d9d550bb0e6",
+    "url": "/karlingcodes/monsters.git/static/js/main.50b2d10f.chunk.js"
+  },
+  {
+    "revision": "af992c510bbc9a7a2530",
+    "url": "/karlingcodes/monsters.git/static/js/runtime-main.6e67bf97.js"
+  }
+]);
